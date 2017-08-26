2a2ab152a7cfe4333a1f492f8dda16894b34352a
al13punk <al13punk@gmail.com>
2017-08-26 21:40:25 +0300

    уровень 2 в процессе

--- 1.JavaSyntax/src/com/javarush/task/task02/task0204/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0204/Solution.java
@@ -1,11 +1,17 @@
 package com.javarush.task.task02.task0204;
 
-/* 
+import org.omg.CORBA.MARSHAL;
+
+/*
 О семейных отношениях
 */
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
     }
 
     public static class Man {
--- 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
@@ -6,5 +6,8 @@ package com.javarush.task.task02.task0207;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int a = 5, b = 7;
+        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
